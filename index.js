ecd880d7df93037b2a0b0ea9465805a706137952
Lu Ye <yelu27@gmail.com>
2017-12-31 18:02:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,15 +18,23 @@ module.exports = {
         math: {
             shortcuts: {
                 parsers: ["markdown", "asciidoc", "restructuredtext"],
-                start: "$$",
-                end: "$$"
+                start: "$",
+                end: "$"
             },
             process: function(blk) {
                 var tex = blk.body;
-                var isInline = !(tex[0] == "\n");
-                var output = katex.renderToString(tex, {
+                var isInline = !(tex[0] == "$");
+                var output = "";
+                if(isInline){
+                    output = katex.renderToString(tex, {
                     displayMode: !isInline
-                });
+                    });
+                }
+                else{
+                    output = katex.renderToString(tex.substr(1, tex.length - 2), {
+                    displayMode: !isInline
+                    });
+                }
 
                 return output;
             }
